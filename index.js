031be6b6a0fbc5d7ebd6071853a43b2a007ce25c
Lakhan Sharma <72928587+izroth@users.noreply.github.com>
2023-08-28 11:27:24 +0530

    Add files via upload
    added

---
+++ index.js
@@ -0,0 +1,43 @@
+const exprss = require('express');
+const fs = require('fs');
+const app = exprss();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/video', (req, res) => {
+    const range = req.headers.range;
+    
+    if (!range) {
+        res.status(400).send('Requires Range header');
+    }
+    const videoPath = 'Newvideo.mp4';
+    const videoSize = fs.statSync('Newvideo.mp4').size;
+
+    const CHUNK_SIZE = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+    const contentLength = end - start + 1;
+    const headers = {
+        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': contentLength,
+        'Content-Type': 'video/mp4',
+    };
+    res.writeHead(206, headers);
+
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+
+    videoStream.pipe(res);
+}
+
+)
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  
+}
+)
+
